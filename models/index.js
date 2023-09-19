d702a2306b8b197af1ea9b2ee403d70d095a93ea
Chase <chasegarrett@tutanota.com>
2023-09-18 21:19:50 -0500

    updated associations

--- models/index.js
+++ models/index.js
@@ -3,7 +3,10 @@ const User = require("./users");
 const Character = require("./characters");
 
 // create associations
-// User.hasMany(DndCharacter, {
+User.hasMany(Character, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE"
+});
 
 // export the module
 module.exports = { User, Character };
