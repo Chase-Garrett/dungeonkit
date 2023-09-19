75fae8c238a599acd3c78a562636ec2028f36eb1
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-09-18 20:10:48 -0500

    set up character model

---
+++ models/characters.js
@@ -0,0 +1,75 @@
+const { Model, DataTypes } = require("sequelize");
+const { v4: uuidv4 } = require("uuid");
+const sequelize = require("../config/connection");
+
+class Characters extends Model {}
+
+Characters.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    race: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subrace: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    class: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    background: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    strStat: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 8,
+    },
+    dexStat: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 8,
+    },
+    conStat: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 8,
+    },
+    wisStat: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 8,
+    },
+    chaStat: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 8,
+    },
+    intStat: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 8,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "characters",
+  }
+);
+
+module.exports = Characters;
