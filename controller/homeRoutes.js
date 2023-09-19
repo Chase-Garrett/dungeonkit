2fa7ec6c55a438feaf22c70bd6538f8b579a6def
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-09-18 21:28:22 -0500

    fixed no trailing ) in get function

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,17 +1,17 @@
-const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
 // get login page
-router.get('/', async (req, res) => {
-	// if the user is already logged in, redirect the request to another route
-	if (req.session.loggedIn) {
-		res.redirect("/");
-		return;
-	}
+router.get("/", async (req, res) => {
+  // if the user is already logged in, redirect the request to another route
+  if (req.session.loggedIn) {
+    res.redirect("/profile");
+    return;
+  }
 
-	res.render("login");
-}
+  res.render("login");
+});
 
 // export the router
 module.exports = router;
