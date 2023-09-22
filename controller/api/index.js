d9e290bda7490feb52bdd563b06dd32c56ed4b4d
Chase <chasegarrett@tutanota.com>
2023-09-21 19:46:26 -0500

    add puppeteer routes to index

--- controller/api/index.js
+++ controller/api/index.js
@@ -4,11 +4,15 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 // import characterRoutes
 const characterRoutes = require("./characterRoutes");
+// import puppeteerRoutes
+const puppeteerRoutes = require("./puppeteerRoutes");
 
 // use the userRoutes
 router.use("/users", userRoutes);
 // use the characterRoutes
 router.use("/characters", characterRoutes);
+// use the puppeteerRoutes
+router.use("/puppeteer", puppeteerRoutes);
 
 // export the module
 module.exports = router;
