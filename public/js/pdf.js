daa8b2f7aeba3a022c1a192f45b0fe61266ba128
Chase <chasegarrett@tutanota.com>
2023-09-25 22:30:16 -0500

    update pdf script

--- public/js/pdf.js
+++ public/js/pdf.js
@@ -3,24 +3,20 @@ const generatePDF = async () => {
   event.preventDefault();
 
   // collect html from character sheet
-  const charsheet = document.querySelector(".charsheet").innerHTML;
-  //console.log(html);
+  //const charId = window.location.pathname.split("/").pop();
 
-  if (charsheet) {
-    // send POST request to API endpoint
-    const response = await fetch("/api/puppeteer/print", {
-      method: "POST",
-      body: JSON.stringify({ charsheet }),
-      headers: { "Content-Type": "application/json" }
-    });
+  //if (charId) {
+  // send POST request to API endpoint
+  const response = await fetch("/api/puppeteer/print", {
+    method: "POST",
+    body: JSON.stringify({}),
+    headers: { "Content-Type": "application/json" }
+  });
 
-    if (response.ok) {
-      // if successful, download PDF
-      document.location.replace("/profile");
-    } else {
-      alert(response.statusText);
-    }
+  if (!response.ok) {
+    alert(response.statusText);
   }
+  //}
 };
 
 document.querySelector(".charsheet").addEventListener("submit", generatePDF);
