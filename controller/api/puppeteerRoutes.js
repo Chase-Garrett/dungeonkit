8cef42ddd8c40e34b0cd209b285a06b6839b8775
Chase <chasegarrett@tutanota.com>
2023-09-25 19:52:08 -0500

    updates to puppeteer controller

--- controller/api/puppeteerRoutes.js
+++ controller/api/puppeteerRoutes.js
@@ -6,15 +6,18 @@ const puppeteer = require("puppeteer");
 // create route for puppeteer to print pdf
 router.post("/print", async (req, res) => {
   // get data from req.body
-  const { html } = req.body;
+  const { charsheet } = req.body;
   // create browser instance
   const browser = await puppeteer.launch({ headless: true });
   // create page instance
   const page = await browser.newPage();
   // set page content to html
-  await page.setContent(html);
+  await page.setContent(charsheet);
+  // reference css file
+  await page.emulateMediaType("screen");
   // create pdf
   const pdf = await page.pdf({
+    path: "charsheet.pdf",
     format: "A4",
     printBackground: true,
     margin: {
