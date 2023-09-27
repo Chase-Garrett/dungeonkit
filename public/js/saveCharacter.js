3048dce1a5e629b02d2fb04375fbc1ea8d8d0d3f
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-09-27 18:26:51 -0500

    got updating existing character working

--- public/js/saveCharacter.js
+++ public/js/saveCharacter.js
@@ -1,25 +1,42 @@
 const saveCharacter = async () => {
-    const playerName = document.getElementById("playername").value.trim();
-    const charName = document.getElementById("charname").value.trim();
-    const race = document.getElementById("race").value.trim();
-    const className = document.getElementById("classlevel").value.trim();
-  
+  const playerName = document.getElementById("playername").value.trim();
+  const charName = document.getElementById("charname").value.trim();
+  const race = document.getElementById("race").value.trim();
+  const className = document.getElementById("classlevel").value.trim();
+  const characterId = document.getElementById("charsheet").dataset.id;
+
+  if (characterId) {
     try {
-      const response = await fetch("/api/characters", {
-        method: "POST",
+      const response = await fetch(`/api/characters/${characterId}`, {
+        method: "PUT",
         body: JSON.stringify({ playerName, charName, race, className }),
         headers: { "Content-Type": "application/json" },
       });
-  
       if (response.ok) {
-        alert("Character Saved!");
+        alert("Character Updated!");
         document.location.replace("/profile");
       } else {
         alert(response.statusText);
       }
     } catch (err) {
-      alert(err);
+      alert(response.statusText);
     }
-  };
+  } else {
+    const response = await fetch("/api/characters", {
+      method: "POST",
+      body: JSON.stringify({ playerName, charName, race, className }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-document.querySelector("#save-content").addEventListener("click", saveCharacter);
+    if (response.ok) {
+      alert("Character Saved!");
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector("#save-content")
+  .addEventListener("click", saveCharacter);
