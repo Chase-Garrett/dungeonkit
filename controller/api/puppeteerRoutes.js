9f09bf34de6e9759ade30183521fe2d51542e676
Chase <chasegarrett@tutanota.com>
2023-09-27 14:43:50 -0500

    adjust parameters to routes

--- controller/api/puppeteerRoutes.js
+++ controller/api/puppeteerRoutes.js
@@ -14,21 +14,39 @@ router.post("/print", async (req, res) => {
   await page.setExtraHTTPHeaders({
     Cookie: req.headers.cookie
   });
-  await page.goto("http://localhost:3001/character");
+  await page.goto("http://localhost:3001/character/");
   // reference css file
   await page.emulateMediaType("screen");
   // create pdf
   await page.pdf({
-    path: "charsheet.pdf",
+    path: "./pdfs/charsheet.pdf",
     format: "A4",
     printBackground: true,
     scale: 0.75
   });
   // close browser
   await browser.close();
-  res.setHeader("Content-Disposition", "attachment; filename=charsheet.pdf");
-  res.setHeader("Content-Type", "application/pdf");
-  res.sendFile(path.resolve(__dirname, "../../charsheet.pdf"));
+  // return response
+  res.json({ message: "PDF created!" });
+});
+
+// create route to download pdf
+router.get("/download", (req, res) => {
+  // res.setHeader("Content-Disposition", "attachment; filename=charsheet.pdf");
+  // res.setHeader("Content-Type", "application/pdf");
+  res
+    .status(200)
+    .download(
+      path.resolve(__dirname, "../../pdfs/charsheet.pdf"),
+      "charsheet.pdf",
+      function (err) {
+        if (err) {
+          res.send({ error: "Error" });
+        } else {
+          console.log("Sent: File");
+        }
+      }
+    );
 });
 
 // export router
