2a513b5bf4cf8354ec2a692d17e1e18e53cff3f1
Chase <chasegarrett@tutanota.com>
2023-09-27 14:43:39 -0500

    add download script

---
+++ public/js/downloadpdf.js
@@ -0,0 +1,11 @@
+// call to download PDF
+const downloadPDF = async () => {
+  event.preventDefault();
+
+  await fetch("/api/puppeteer/download", {
+    method: "GET",
+    headers: { "Content-Type": "application/pdf" }
+  });
+};
+
+document.querySelector("#download").addEventListener("click", downloadPDF);
