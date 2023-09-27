6822978dbb45390406678a72cc5e7454b46eb071
Chase <chasegarrett@tutanota.com>
2023-09-27 14:43:14 -0500

    adjust parameters to pdf script

--- public/js/pdf.js
+++ public/js/pdf.js
@@ -5,7 +5,7 @@ const generatePDF = async () => {
   // collect html from character sheet
   //const charId = window.location.pathname.split("/").pop();
 
-  //if (charId) {
+  // if (charId) {
   // send POST request to API endpoint
   const response = await fetch("/api/puppeteer/print", {
     method: "POST",
@@ -16,32 +16,7 @@ const generatePDF = async () => {
   if (!response.ok) {
     alert(response.statusText);
   }
-  //}
-};
-
-const saveCharacter = async () => {
-  const charName = document.getElementById("charname").value.trim();
-
-  try {
-    const response = await fetch("/api/characters", {
-      method: "POST",
-      body: JSON.stringify({ charName }),
-      headers: { "Content-Type": "application/json" }
-    });
-
-    if (response.ok) {
-      alert("Character Saved!");
-      document.location.replace("/profile");
-    } else {
-      alert(response.statusText);
-    }
-  } catch (err) {
-    alert(err);
-  }
+  // }
 };
 
 document.querySelector(".charsheet").addEventListener("submit", generatePDF);
-
-document
-  .querySelector("#save-content")
-  .addEventListener("click", saveCharacter);
